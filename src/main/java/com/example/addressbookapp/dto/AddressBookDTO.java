038f96b027618b382077f8c8219c3ff2cf2ac5fe
Kshitija Patil <patilkshitija2@gmail.com>
2021-10-01 00:23:16 +0530

    [kshitija]:validation done in addressbook DTO

--- src/main/java/com/example/addressbookapp/dto/AddressBookDTO.java
+++ src/main/java/com/example/addressbookapp/dto/AddressBookDTO.java
@@ -2,6 +2,7 @@ package com.example.addressbookapp.dto;
 
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import lombok.ToString;
@@ -14,11 +15,22 @@ public @ToString class AddressBookDTO {
 	@Pattern(regexp ="^[A-Z]{1}[a-zA-z\\s]{2,}$",message="lastname Invalid")
     public String lastName;
 	
+	@NotNull(message="address should not be empty")
 	public String address;
+	
+	@NotNull(message="city should not be empty")
 	public String city;
+	
+	@NotNull(message="state should not be empty")
 	public String state;
+	
+	@NotNull(message="zipcode should not be empty")
 	public int zipcode;
+	
+	@Pattern(regexp="(0/91)?[7-9][0-9]{9}",message="Phone No Invalid")
 	public String phoneNo;
+	
+	@NotNull(message="relations should not be empty")
 	public List<String> relations;
 	
 	}
--- src/main/java/com/example/addressbookapp/exception/AddressBookExeptionHandler.java
+++ src/main/java/com/example/addressbookapp/exception/AddressBookExeptionHandler.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,27 +13,36 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.example.addressbookapp.dto.ResponseDTO;
 
-
+import lombok.extern.slf4j.Slf4j;
 
 @ControllerAdvice
+@Slf4j
 public class AddressBookExeptionHandler {
-	
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ResponseDTO>handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
-		List<ObjectError>errorList=exception.getBindingResult().getAllErrors();
-	List<String>errMsg=errorList.stream().map(objerr ->objerr.getDefaultMessage()).collect(Collectors.toList());
-	ResponseDTO responseDto=new ResponseDTO("Exception while prossesing rest request",errMsg);
-	return new ResponseEntity<ResponseDTO>(responseDto , HttpStatus.BAD_REQUEST);
-	
-	
+	private final String message = "Exception while processing rest request";
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<ResponseDTO> handHttpMessageException(HttpMessageNotReadableException exception) {
+		log.error("Invalid Phone no", exception);
+		ResponseDTO responseDto = new ResponseDTO(message, "phone should be 10 numeric value");
+		return new ResponseEntity<ResponseDTO>(responseDto, HttpStatus.BAD_REQUEST);
 	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
+			MethodArgumentNotValidException exception) {
+		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+		List<String> errMsg = errorList.stream().map(objerr -> objerr.getDefaultMessage()).collect(Collectors.toList());
+		ResponseDTO responseDto = new ResponseDTO(message, errMsg);
+		return new ResponseEntity<ResponseDTO>(responseDto, HttpStatus.BAD_REQUEST);
+
+	}
+
 	@ExceptionHandler(AddressBookException.class)
-	public ResponseEntity<ResponseDTO>handleEmployeePayrollException(AddressBookException exception){
-		
-	ResponseDTO responseDto=new ResponseDTO("Exception while prossesing rest request",exception.getMessage());
-	return new ResponseEntity<ResponseDTO>(responseDto , HttpStatus.BAD_REQUEST);
-	
-	
+	public ResponseEntity<ResponseDTO> handleEmployeePayrollException(AddressBookException exception) {
+
+		ResponseDTO responseDto = new ResponseDTO(message, exception.getMessage());
+		return new ResponseEntity<ResponseDTO>(responseDto, HttpStatus.BAD_REQUEST);
+
 	}
 
 }
